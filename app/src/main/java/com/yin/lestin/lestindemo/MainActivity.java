807ad691c5534ca5823e48812c1931856016df58
lestinYin <lestin.yin@gmail.com>
2017-05-22 16:36:59 +0800

    1111

--- app/src/main/java/com/yin/lestin/lestindemo/MainActivity.java
+++ app/src/main/java/com/yin/lestin/lestindemo/MainActivity.java
@@ -3,6 +3,9 @@ package com.yin.lestin.lestindemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rx.Observable;
+import rx.Subscriber;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.print("ttt");
+        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello");
+                subscriber.onNext("Hi");
+                subscriber.onNext("Aloha");
+                subscriber.onCompleted();
+            }
+        });
     }
 }
