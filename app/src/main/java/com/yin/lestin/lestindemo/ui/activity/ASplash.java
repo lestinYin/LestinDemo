a155dcf06a181a5dc043005b5083a5aa4cd83f44
lestinYin <lestin.yin@gmail.com>
2017-05-27 09:53:43 +0800

    1111

---
+++ app/src/main/java/com/yin/lestin/lestindemo/ui/activity/ASplash.java
@@ -0,0 +1,8 @@
+package com.yin.lestin.lestindemo.ui.activity;
+
+/**
+ * Created by Administrator on 2017/5/23.
+ */
+
+public class ASplash {
+}
---
+++ app/src/main/java/com/yin/lestin/lestindemo/ui/activity/MainActivity.java
@@ -0,0 +1,120 @@
+package com.yin.lestin.lestindemo.ui.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.scu.miomin.shswiperefresh.core.SHSwipeRefreshLayout;
+import com.scu.miomin.shswiperefresh.view.SHListView;
+import com.yin.lestin.lestindemo.BuildConfig;
+import com.yin.lestin.lestindemo.R;
+import com.yin.lestin.lestindemo.ui.adapter.SimpleLvAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<String> mDatas;
+    private SHSwipeRefreshLayout swipeRefreshLayout;
+    private SHListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+        initLv();
+        initSwipeRefreshLayout();
+
+    }
+
+    public static void startActivity(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
+    }
+
+    private void initLv() {
+        lv = (SHListView) findViewById(R.id.lv);
+        lv.setAdapter(new SimpleLvAdapter(this,mDatas));
+    }
+
+    private void initSwipeRefreshLayout() {
+        swipeRefreshLayout = (SHSwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+        final View view = inflater.inflate(R.layout.refresh_view, null);
+        final TextView textView = (TextView) view.findViewById(R.id.title);
+        swipeRefreshLayout.setFooterView(view);
+        swipeRefreshLayout.setOnRefreshListener(new SHSwipeRefreshLayout.SHSOnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.finishRefresh();
+                        Toast.makeText(MainActivity.this, "刷新完成", Toast.LENGTH_SHORT).show();
+                    }
+                }, 1600);
+            }
+
+            @Override
+            public void onLoading() {
+                swipeRefreshLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.finishLoadmore();
+                        lv.setEnabled(true);
+                        Toast.makeText(MainActivity.this, "加载完成", Toast.LENGTH_SHORT).show();
+                    }
+                }, 1600);
+            }
+
+            /**
+             * 监听下拉刷新过程中的状态改变
+             * @param percent 当前下拉距离的百分比（0-1）
+             * @param state 分三种状态{NOT_OVER_TRIGGER_POINT：还未到触发下拉刷新的距离；OVER_TRIGGER_POINT：已经到触发下拉刷新的距离；START：正在下拉刷新}
+             */
+            @Override
+            public void onRefreshPulStateChange(float percent, int state) {
+                switch (state) {
+                    case SHSwipeRefreshLayout.NOT_OVER_TRIGGER_POINT:
+                        swipeRefreshLayout.setRefreshViewText("下拉刷新");
+                        break;
+                    case SHSwipeRefreshLayout.OVER_TRIGGER_POINT:
+                        swipeRefreshLayout.setRefreshViewText("松开刷新");
+                        break;
+                    case SHSwipeRefreshLayout.START:
+                        swipeRefreshLayout.setRefreshViewText("正在刷新");
+                        break;
+                }
+            }
+
+            @Override
+            public void onLoadmorePullStateChange(float percent, int state) {
+                switch (state) {
+                    case SHSwipeRefreshLayout.NOT_OVER_TRIGGER_POINT:
+                        textView.setText("上拉加载");
+                        break;
+                    case SHSwipeRefreshLayout.OVER_TRIGGER_POINT:
+                        textView.setText("松开加载");
+                        break;
+                    case SHSwipeRefreshLayout.START:
+                        lv.setEnabled(false);
+                        textView.setText("正在加载...");
+                        break;
+                }
+            }
+        });
+    }
+
+    protected void initData() {
+        mDatas = new ArrayList();
+        for (int i = 'A'; i < 'z'; i++) {
+            mDatas.add("" + (char) i);
+        }
+    }
+}
